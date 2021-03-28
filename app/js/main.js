af7790cfd8cb331276e3345760ebf060aba0078d
Wisem4n <kstepanov55@gmail.com>
2021-03-28 08:54:00 +0600

    added shop pages

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,37 @@
 $(function () {
 
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: '$',
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+  
+  $(function () {
+    $(".star").rateYo({
+      rating: 3.6,
+      readOnly: true,
+      starWidth: "11px",
+      normalFill: "#d6d6d6",
+      ratedFill: "#ffcc00"
+    });
+  });
+
+  $(function () {
+    $(".star-list").rateYo({
+      readOnly: true,
+      starWidth: "16px",
+      normalFill: "#d6d6d6",
+      ratedFill: "#ffcc00"
+    });
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false
